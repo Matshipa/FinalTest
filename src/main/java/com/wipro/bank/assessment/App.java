de0f90bb4d734348e1d19e542bfb345ab0952d56
Matshipa <barnad.matshipa1@wipro.com>
2017-05-10 14:14:16 +0200

    Final Java Test

--- src/main/java/com/wipro/bank/assessment/App.java
+++ src/main/java/com/wipro/bank/assessment/App.java
@@ -1,13 +1,61 @@
 package com.wipro.bank.assessment;
+import com.wipro.bank.entity.Account;
 
-/**
- * Hello world!
- *
- */
+import java.util.*;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    
+	
+		
+	public static List<Account> accounts = new ArrayList<Account>();
+	
+	public static List<Account> getAccounts() {
+		return accounts;
+	}
+
+  
+    public Account getAccountDetailsByID(int id){
+    	
+    	Account account = null;
+    	
+    	if(accounts!=null)
+    	{
+    		for(Account acc: accounts)
+    		{
+    			if(acc.getAccountID()==id)
+    			{
+    				account = acc;
+    				break;
+    			}
+    			
+    		}
+    	}
+    
+    	
+    	return account;
     }
+    public List<Account> getAccountDetailsByBalance(double amount)
+    {
+    	List<Account> accountBalance = new ArrayList<Account>();
+    	boolean flag = true;
+    	
+    		for(Account acc: accounts)
+    		{
+    			
+    			if(acc.getBalance()>=amount)
+    			{	
+    				accountBalance.add(acc);
+    				flag = false;
+    				
+    			}
+    			
+    		}
+    		
+    	if(flag)
+    		return null;
+    	else   	
+    		return accountBalance;
+    }
+
 }
